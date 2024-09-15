ccee9cacc0777ea45932e613dfad6562fcb5b8d0
Prashant <royprashant800@gmail.com>
2024-09-15 23:57:42 +0530

    location fix

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,4 +1,5 @@
-const baseUrl = location.hostname === "localhost" ? "http://localhost:2000" : "https://pluskart-rest-server-8a6894d0725f.herokuapp.com/";
+// const baseUrl = location.hostname === "localhost" ? "http://localhost:2000" : "https://pluskart-rest-server-8a6894d0725f.herokuapp.com/";
+const baseUrl = "https://pluskart-rest-server-8a6894d0725f.herokuapp.com/";
 
 export const api = `${baseUrl}/api`;
 export const generatePublicUrl = (filename) => {
