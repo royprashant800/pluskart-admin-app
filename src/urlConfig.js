2e3d38c950774e6a4e4ddc6f4f76332e9373915a
Prashant <royprashant800@gmail.com>
2025-07-17 15:17:28 +0530

    url debug

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,5 +1,5 @@
 // const baseUrl = "http://localhost:2000";
-const baseUrl = "https://pluskart-rest-server.vercel.app/";
+const baseUrl = "https://pluskart-rest-server.vercel.app";
 
 export const api = `${baseUrl}/api`;
 export const generatePublicUrl = (filename) => {
