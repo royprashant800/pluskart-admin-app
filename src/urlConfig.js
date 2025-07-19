3ba7cd468bd91034b82991dc39f95b01086cbcfc
Prashant <royprashant800@gmail.com>
2025-07-19 16:29:45 +0530

    url changes

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,5 +1,5 @@
 // const baseUrl = "http://localhost:2000";
-const baseUrl = "https://pluskart-rest-server.vercel.app";
+const baseUrl = "https://web-production-9bb41.up.railway.app";
 
 export const api = `${baseUrl}/api`;
 export const generatePublicUrl = (filename) => {
