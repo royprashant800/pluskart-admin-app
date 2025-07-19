b9e5cd9ec8d1bc072e9d3ef9b2e247b167398cb8
Prashant <royprashant800@gmail.com>
2025-07-19 17:35:41 +0530

    header add

--- src/actions/user.actions.js
+++ src/actions/user.actions.js
@@ -2,29 +2,35 @@ import axios from "../helpers/axios";
 import { authConstants, userConstants } from "./constants";
 
 export const signup = (user) => {
+  console.log(user);
 
-    console.log(user);
+  return async (dispatch) => {
+    dispatch({ type: userConstants.USER_REGISTER_REQUEST });
+    const res = await axios.post(
+      "admin/signup",
+      {
+        ...user,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
-    return async (dispatch) => {
-
-        dispatch({ type: userConstants.USER_REGISTER_REQUEST })
-        const res = await axios.post('admin/signup', {
-            ...user
-        })
-
-        if (res.status === 201) {
-            const { message } = res.data;
-            dispatch({
-                type: userConstants.USER_REGISTER_SUCCESS,
-                payload: { message }
-            })
-        } else {
-            if (res.status === 400) {
-                dispatch({
-                    type: userConstants.USER_REGISTER_FAILURE,
-                    payload: { error: res.data.error }
-                })
-            }
-        }
+    if (res.status === 201) {
+      const { message } = res.data;
+      dispatch({
+        type: userConstants.USER_REGISTER_SUCCESS,
+        payload: { message },
+      });
+    } else {
+      if (res.status === 400) {
+        dispatch({
+          type: userConstants.USER_REGISTER_FAILURE,
+          payload: { error: res.data.error },
+        });
+      }
     }
-}
+  };
+};
